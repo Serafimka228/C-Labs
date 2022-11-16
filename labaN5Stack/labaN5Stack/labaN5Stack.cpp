e1b38977802eb8a1cfda6cb99394e81b8a443d94
Serafim Leybutin <leybutin228@mail.ru>
2022-11-16 17:45:06 +0300

    stack

--- labaN5Stack/labaN5Stack/labaN5Stack.cpp
+++ labaN5Stack/labaN5Stack/labaN5Stack.cpp
@@ -36,6 +36,11 @@ public:
 		Arr[_numberOfElement] = value;
 	}
 
+	int Size()
+	{
+		return _capacity;
+	}
+
 	type Pop()
 	{
 		if (IsEmpty())
@@ -53,20 +58,94 @@ private:
 	type* Arr;
 };
 
+template <> class Stack<char*>
+{
+public:
+	Stack(int capacity) :
+		_capacity(capacity), _numberOfElement(-1)
+	{
+		Arr = new char*[_capacity];
+	}
+
+	~Stack()
+	{
+		delete[] Arr;
+	}
+
+	bool IsEmpty()
+	{
+		return _numberOfElement < 0;
+	}
+
+	bool IsFull()
+	{
+		return _numberOfElement >= _capacity - 1;
+	}
+
+	void Push(char* value)
+	{
+		if (IsFull())
+		{
+			std::cout << "The stack is full" << std::endl;
+			return;
+		}
+
+		_numberOfElement++;
+		Arr[_numberOfElement] = value;
+	}
+
+	char* Pop()
+	{
+		if (IsEmpty())
+		{
+			std::cout << "The stack is empty" << std::endl;
+			return 0;
+		}
+		_numberOfElement--;
+		return Arr[_numberOfElement + 1];
+	}
+
+private:
+	int _capacity;
+	int _numberOfElement;
+	char** Arr;
+};
+
+
 int main()
 {
-	Stack<char> MyStack(5);
-	MyStack.Pop();
-	MyStack.Push('K');
-	MyStack.Push('O');
-	MyStack.Push('T');
-	MyStack.Push('I');
-	MyStack.Push('K');
-	MyStack.Push('O');
+	Stack<char> MyStack1(5);
+	MyStack1.Pop();
+	MyStack1.Push('K');
+	MyStack1.Push('I');
+	MyStack1.Push('T');
+	MyStack1.Push('O');
+	MyStack1.Push('K');
+	MyStack1.Push('O');
 
 	for (int i = 0; i < 5; i++)
 	{
-		std::cout << MyStack.Pop() << std::endl;
+		std::cout << MyStack1.Pop() << std::endl;
+	}
+
+	std::cout << "-------------------------------" << std::endl;
+
+	Stack<char*> MyStack2(3);
+
+	char c = 'C';
+	char* C = &c;
+	char a = 'A';
+	char* A = &a;
+	char t = 'T';
+	char* T = &t;
+	MyStack2.Push(T);
+	MyStack2.Push(A);
+	MyStack2.Push(C);
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << *MyStack2.Pop() << std::endl;
 	}
 
 }
